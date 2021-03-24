fe3521d22600597efbfdaa0f9adbe7da6b697247
Riyazahmed K <riyazahmed.k@in.abb.com>
2021-03-24 13:20:32 +0530

    added database settings in application

--- BooksApi/Startup.cs
+++ BooksApi/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksApi.Models;
+using Microsoft.Extensions.Options;
 
 namespace BooksApi
 {
@@ -25,6 +27,12 @@ namespace BooksApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<BookstoreDatabaseSettings>(
+                Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
+
+            services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
             services.AddControllers();
         }
 
